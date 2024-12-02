2fb6b90cd73dcd53e1a8513ab2641af4d6e6d1d3
ScualoGiocatore <marcellettimatteo02@gmail.com>
2024-12-02 16:45:45 +0100

    Oh shit, here we go again

---
+++ service/api/post-create-post.go
@@ -0,0 +1,8 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/scualogiocatore/WasaPhoto/service/struct"
+)
---
+++ service/struct/UserStruct.go
@@ -0,0 +1,7 @@
+package _struct
+
+type User struct {
+	UserID   int    `json:"userID"`
+	Username string `json:"username"`
+	Propic   string `json:"propic"`
+}
